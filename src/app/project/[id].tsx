4e1047350e2ce965569e800c6454f1da5d57924f
Argifari <argifari40@gmail.com>
2026-09-08 14:59:19 +0700

    fix: UI bug in [id].tsx, flex direction and wrong section

--- src/app/project/[id].tsx
+++ src/app/project/[id].tsx
@@ -94,7 +94,7 @@ export default function ProjectDetailScreen() {
   };
 
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <TouchableOpacity
           onPress={() => router.back()}
@@ -106,49 +106,55 @@ export default function ProjectDetailScreen() {
         <View style={{ width: 60 }} />
       </View>
 
-      <ScrollView>
+      <ScrollView showsHorizontalScrollIndicator={false}>
         {/** SECTION TASK */}
         <View style={styles.section}>
-          <View style={styles.sectionHeader}>
-            <Text style={styles.sectionTitle}>Folder Text</Text>
-            <TouchableOpacity onPress={() => setTaskModalVisible(true)}>
-              <AddIcon width={20} height={20} />
-            </TouchableOpacity>
-
-            {tasks.length === 0 ? (
-              <Text style={styles.emptyText}>none</Text>
-            ) : (
-              tasks.map((task) => (
-                <TaskCard
-                  key={task.id}
-                  task={task}
-                  onToggleStatus={handleToggleTask}
-                  onDelete={handleDeleteTask}
-                />
-              ))
-            )}
+          <View
+            style={[styles.sectionHeader, { justifyContent: "space-between" }]}
+          >
+            <View style={styles.sectionTitleRow}>
+              <Text style={styles.sectionTitle}>Folder Text</Text>
+              <TouchableOpacity onPress={() => setTaskModalVisible(true)}>
+                <AddIcon width={20} height={20} />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.activeCount}>
+              {(tasks || []).filter((t) => t.doneStatus === 0).length} Active
+            </Text>
           </View>
+          {tasks.length === 0 ? (
+            <Text style={styles.emptyText}>none</Text>
+          ) : (
+            tasks.map((task) => (
+              <TaskCard
+                key={task.id}
+                task={task}
+                onToggleStatus={handleToggleTask}
+                onDelete={handleDeleteTask}
+              />
+            ))
+          )}
 
           {/**SECTION NOTES */}
           <View style={(styles.section, { paddingBottom: 40 })}>
-            <View style={styles.sectionHeader}>
+            <View style={[styles.sectionHeader, { gap: 8 }]}>
               <Text style={styles.sectionTitle}>Folder Notes</Text>
               <TouchableOpacity onPress={() => setNoteModalVisible(true)}>
                 <AddIcon width={20} height={20} />
               </TouchableOpacity>
-              {notes.length === 0 ? (
-                <Text style={styles.emptyText}>None</Text>
-              ) : (
-                notes.map((note) => (
-                  <NoteCard
-                    key={note.id}
-                    note={note}
-                    onPress={() => console.log("Masuk ke note :", note.id)}
-                    onDelete={handleDeleteNote}
-                  />
-                ))
-              )}
             </View>
+            {notes.length === 0 ? (
+              <Text style={styles.emptyText}>None</Text>
+            ) : (
+              notes.map((note) => (
+                <NoteCard
+                  key={note.id}
+                  note={note}
+                  onPress={() => console.log("Masuk ke note :", note.id)}
+                  onDelete={handleDeleteNote}
+                />
+              ))
+            )}
           </View>
         </View>
       </ScrollView>
@@ -197,7 +203,6 @@ const styles = StyleSheet.create({
   section: { marginTop: 24 },
   sectionHeader: {
     flexDirection: "row",
-    justifyContent: "space-between",
     alignItems: "center",
     marginBottom: 16,
   },
@@ -205,6 +210,16 @@ const styles = StyleSheet.create({
     color: COLORS.neutral,
     fontFamily: FONTS.bold,
     fontSize: FONT_SIZES.large,
+  },
+  sectionTitleRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+  activeCount: {
+    color: "#ff8888",
+    fontFamily: FONTS.regular,
+    fontSize: FONT_SIZES.small,
   },
   emptyText: { color: COLORS.gray, marginTop: 10, fontFamily: FONTS.regular },
 });
