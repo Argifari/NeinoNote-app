ded5aff03685fb249c706285a883978d811303f0
Argifari <argifari40@gmail.com>
2026-09-08 08:36:19 +0700

    feat: add [id].tsx to new feature onClick folder in Dashboard complete

--- src/app/project/[id].tsx
+++ src/app/project/[id].tsx
@@ -1,14 +1,29 @@
+import AddIcon from "@/assets/images/addIcon.svg";
 import {
-    addNote,
-    addTask,
-    data,
-    deleteNotes,
-    deleteTask,
-    getDataInProject,
-    updateDoneStatus,
+  addNote,
+  addTask,
+  data,
+  deleteNotes,
+  deleteTask,
+  getDataInProject,
+  updateDoneStatus,
 } from "@/database/taskQueries";
-import { useLocalSearchParams } from "expo-router";
+import { router, useLocalSearchParams } from "expo-router";
 import { useEffect, useState } from "react";
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import { NoteCard } from "@/components/NoteCard";
+import { NoteFormModal } from "@/components/NoteFormModal";
+import { TaskCard } from "@/components/TaskCard";
+import { TaskFormModal } from "@/components/TaskFormModal";
+import { COLORS, FONTS, FONT_SIZES, SPACING } from "@/constants/theme";
 
 export default function ProjectDetailScreen() {
   const { id } = useLocalSearchParams();
@@ -24,6 +39,7 @@ export default function ProjectDetailScreen() {
   const muatDataFolder = () => {
     const hasil: data = getDataInProject(folderId);
 
+    setFolderName(hasil.project[0]?.projectTitle ?? "");
     setTasks(hasil?.tasks || []);
     setNotes(hasil?.notes || []);
   };
@@ -77,5 +93,118 @@ export default function ProjectDetailScreen() {
     muatDataFolder();
   };
 
-  return 0;
+  return (
+    <SafeAreaView>
+      <View style={styles.header}>
+        <TouchableOpacity
+          onPress={() => router.back()}
+          style={styles.backButton}
+        >
+          <Text style={styles.backText}>{" < back"}</Text>
+        </TouchableOpacity>
+        <Text style={styles.headerTitle}>{folderName}</Text>
+        <View style={{ width: 60 }} />
+      </View>
+
+      <ScrollView>
+        {/** SECTION TASK */}
+        <View style={styles.section}>
+          <View style={styles.sectionHeader}>
+            <Text style={styles.sectionTitle}>Folder Text</Text>
+            <TouchableOpacity onPress={() => setTaskModalVisible(true)}>
+              <AddIcon width={20} height={20} />
+            </TouchableOpacity>
+
+            {tasks.length === 0 ? (
+              <Text style={styles.emptyText}>none</Text>
+            ) : (
+              tasks.map((task) => (
+                <TaskCard
+                  key={task.id}
+                  task={task}
+                  onToggleStatus={handleToggleTask}
+                  onDelete={handleDeleteTask}
+                />
+              ))
+            )}
+          </View>
+
+          {/**SECTION NOTES */}
+          <View style={(styles.section, { paddingBottom: 40 })}>
+            <View style={styles.sectionHeader}>
+              <Text style={styles.sectionTitle}>Folder Notes</Text>
+              <TouchableOpacity onPress={() => setNoteModalVisible(true)}>
+                <AddIcon width={20} height={20} />
+              </TouchableOpacity>
+              {notes.length === 0 ? (
+                <Text style={styles.emptyText}>None</Text>
+              ) : (
+                notes.map((note) => (
+                  <NoteCard
+                    key={note.id}
+                    note={note}
+                    onPress={() => console.log("Masuk ke note :", note.id)}
+                    onDelete={handleDeleteNote}
+                  />
+                ))
+              )}
+            </View>
+          </View>
+        </View>
+      </ScrollView>
+
+      {/**MODAL */}
+      <TaskFormModal
+        visible={isTaskModalVisible}
+        projectId={folderId}
+        projectList={[{ id: folderId, projectTitle: folderName }]}
+        onClose={() => setTaskModalVisible(false)}
+        onSave={handleSimpanTugas}
+      />
+      <NoteFormModal
+        visible={isNoteModalVisible}
+        projectId={folderId}
+        projectList={[{ id: folderId, projectTitle: folderName }]}
+        onClose={() => setNoteModalVisible(false)}
+        onSave={handleSimpanNote}
+      />
+    </SafeAreaView>
+  );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.background,
+    paddingHorizontal: 20,
+  },
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: SPACING.large,
+  },
+  backButton: { padding: 8, marginLeft: -8 },
+  backText: {
+    color: COLORS.gray,
+    fontFamily: FONTS.bold,
+    fontSize: FONT_SIZES.medium,
+  },
+  headerTitle: {
+    color: COLORS.primary,
+    fontFamily: FONTS.bold,
+    fontSize: FONT_SIZES.large,
+  },
+  section: { marginTop: 24 },
+  sectionHeader: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 16,
+  },
+  sectionTitle: {
+    color: COLORS.neutral,
+    fontFamily: FONTS.bold,
+    fontSize: FONT_SIZES.large,
+  },
+  emptyText: { color: COLORS.gray, marginTop: 10, fontFamily: FONTS.regular },
+});
